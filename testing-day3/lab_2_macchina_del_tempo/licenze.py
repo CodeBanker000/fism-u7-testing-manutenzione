f5361b5de43c9bf8970011a81232c8607d551d28
CodeBanker000 <codebanker000@gmail.com>
2026-04-20 14:01:44 +0200

    esercizio macchina del tempo

--- testing-day3/lab_2_macchina_del_tempo/licenze.py
+++ testing-day3/lab_2_macchina_del_tempo/licenze.py
@@ -1,4 +1,18 @@
 import datetime
+from unittest.mock import patch
+
+"""
+#"nome_modulo.libreria_importata.funzione"
+@patch('prova.random.randint)
+def test_vittoria(mock_randint):
+    #setup
+    mock_randint.return_value = 6
+    #esecuzione
+    risultato = lancia_dado_magico()
+
+    #verifica
+    assert risultato
+"""
 
 def licenza_scaduta(data_scadenza: str) -> bool:
     """Ritorna True se la licenza è scaduta rispetto a OGGI.
